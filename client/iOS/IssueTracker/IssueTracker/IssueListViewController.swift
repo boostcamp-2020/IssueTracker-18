37d3222c440ff73632726ec9ec6f85f61cdb7e65
alimelon <zhdtns2005@naver.com>
2020-11-02 21:17:30 +0900

    [feat]: collectionView 생성
    - storyboard에 collectionView 추가
    - collectionView List Layout 설정

--- client/iOS/IssueTracker/IssueTracker/IssueListViewController.swift
+++ client/iOS/IssueTracker/IssueTracker/IssueListViewController.swift
@@ -7,17 +7,38 @@
 
 import UIKit
 
-class IssueListViewController: UIViewController {
+class IssueListViewController: UIViewController, UICollectionViewDelegate {
     
+    // MARK: - @IBOutlet Properties
+    @IBOutlet weak var newIssueButton: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        setNavigationBar()
+        configureNavigationBar()
     }
-    
-    private func setNavigationBar() {
+
+    // MARK: - Methods
+    private func configureNavigationBar() {
         self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.barTintColor = .white
+        self.navigationController?.navigationBar.barTintColor = .systemBackground
         self.navigationController?.navigationBar.isTranslucent = false
     }
+    
+    private func configureNewIssueButton() {
+        view.bringSubviewToFront(newIssueButton)
+    }
+    
+    private func configureCollectionView() {
+        collectionView.collectionViewLayout = createLayout()
+        collectionView.delegate = self
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        return UICollectionViewCompositionalLayout.list(using: configuration)
+    }
+    
 }
     
