315182cfd75fbcb228febf0ffe3a0d06354d39ac
alimelon <zhdtns2005@naver.com>
2020-11-03 01:42:29 +0900

    [feat]: 레이블 목록 화면 collectionView layout 설정
    - collectionView 추가
    - list layout 설정
    - UICollectionViewDelegate 채택

--- client/iOS/IssueTracker/IssueTracker/LabelListViewController.swift
+++ client/iOS/IssueTracker/IssueTracker/LabelListViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class LabelListViewController: UIViewController {
+class LabelListViewController: UIViewController, UICollectionViewDelegate {
 
     // MARK: - @IBOutlet Properties
     @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     // MARK: - Life Cycle Methods
     override func viewDidLoad() {
@@ -23,6 +24,16 @@ class LabelListViewController: UIViewController {
         navigationBar.barTintColor = .systemBackground
         navigationBar.isTranslucent = false
         navigationBar.topItem?.title = ""
+    }
+    
+    private func configureCollectionView() {
+        collectionView.collectionViewLayout = createLayout()
+        collectionView.delegate = self
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        return UICollectionViewCompositionalLayout.list(using: configuration)
     }
 
 }
