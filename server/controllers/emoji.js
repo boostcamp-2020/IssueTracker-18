859d7b09ccbe9ce14369332fa928b73d1eba122b
sukstar76 <sukstar76@gmail.com>
2020-11-03 17:45:06 +0900

    refactor: emoji get API 리팩토링
    emoji get API JSON 응답 형식 수정:

--- server/controllers/emoji.js
+++ server/controllers/emoji.js
@@ -4,17 +4,13 @@ const wrapAsync = require('@utils/async-wrapper');
 
 const router = express.Router();
 const { models } = sequelize;
-const emojiModel = models.emoji;
 
 router.get(
   '/',
   wrapAsync(async (req, res, next) => {
-    const emojis = await emojiModel.findAll({ attributes: ['id', 'name', 'imageUrl'] });
+    const emojis = await models.emoji.findAll({ attributes: ['id', 'name', 'imageUrl'] });
 
-    return res.status(200).json({
-      status: 200,
-      result: emojis,
-    });
+    return res.status(200).json(emojis);
   }),
 );
 
