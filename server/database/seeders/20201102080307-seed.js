ec5384409a3ba59777ac9e179d5ca63af04c460c
grap3fruit <swkwon333@gmail.com>
2020-11-03 11:50:44 +0900

    feat: seeder 데이터 추가, issue 아래로 이동

--- server/database/seeders/20201102080307-seed.js
+++ server/database/seeders/20201102080307-seed.js
@@ -20,18 +20,35 @@ const issues = [
   {
     title: 'first issue',
     createrId: 1,
+    milestoneId: 1,
     createdAt: new Date(),
     updatedAt: new Date(),
   },
   {
     title: 'second issue',
     createrId: 1,
+    milestoneId: 1,
     createdAt: new Date(),
     updatedAt: new Date(),
   },
   {
     title: 'third issue',
+    createrId: 2,
+    milestoneId: 2,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    title: 'four 이슈!',
+    createrId: 3,
+    milestoneId: 2,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    title: '다섯번째 이슈!@',
     createrId: 1,
+    milestoneId: 1,
     createdAt: new Date(),
     updatedAt: new Date(),
   },
@@ -172,9 +189,9 @@ const commentEmoji = [
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('users', users, {});
-    await queryInterface.bulkInsert('issues', issues, {});
     await queryInterface.bulkInsert('labels', labels, {});
     await queryInterface.bulkInsert('milestones', milestones, {});
+    await queryInterface.bulkInsert('issues', issues, {});
     await queryInterface.bulkInsert('comments', comments, {});
     await queryInterface.bulkInsert('issueAssignee', issueAssignee, {});
     await queryInterface.bulkInsert('issueLabel', issueLabel, {});
@@ -184,9 +201,9 @@ module.exports = {
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('users', null, {});
-    await queryInterface.bulkDelete('issues', null, {});
     await queryInterface.bulkDelete('labels', null, {});
     await queryInterface.bulkDelete('milestones', null, {});
+    await queryInterface.bulkDelete('issues', null, {});
     await queryInterface.bulkDelete('comments', null, {});
     await queryInterface.bulkDelete('issueAssignee', null, {});
     await queryInterface.bulkDelete('issueLabel', null, {});
