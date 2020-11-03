aceaa963f0b5d2d75686b2edbcde39fa00ec38c1
alimelon <zhdtns2005@naver.com>
2020-11-03 17:14:27 +0900

    fix: 레이블 목록 collectionView 제거

--- client/iOS/IssueTracker/IssueTracker/LabelListViewController.swift
+++ client/iOS/IssueTracker/IssueTracker/LabelListViewController.swift
@@ -11,38 +11,16 @@ class LabelListViewController: UIViewController, UICollectionViewDelegate {
 
     // MARK: - @IBOutlet Properties
     @IBOutlet weak var navigationBar: UINavigationBar!
-    @IBOutlet weak var collectionView: UICollectionView!
+ 
     
-    @IBAction func toucedPlusButton(_ sender: UIBarButtonItem) {
-        collectionView.isEditing = !collectionView.isEditing
-        
-//        if let currentIndexPath = self.dataSource.indexPath(for: item) {
-//            if let cell = self.collectionView.cellForItem(at: currentIndexPath) as? UICollectionViewListCell {
-//                UIView.animate(withDuration: 0.2) {
-//                    cell.accessories = self.accessoriesForListCellItem(item)
-//                }
-//            }
-//        }
-//        collectionView.visibleCells.forEach { cell in
-//            if cell == cell as? UICollectionViewListCell {
-//                cell.accessoty
-//            }
-//
-//        }
-    }
-    
-    // MARK: - Properties
-    var dataSource: UICollectionViewDiffableDataSource<Section, Label>!
-    var labels = [Label(title: "feat", description: "기능에 대한 레이블 입니다dfafafaafafadfah", color: "ㅇㅇ"),
-                  Label(title: "bug", description: "수정할 버그에 대한 레이블 입니다", color: "ㅇㅇ")]
+
+    var labels = [Label(id: 1, title: "feat", description: "기능에 대한 레이블 입니다dfafafaafafadfah", color: "ㅇㅇ"),
+                  Label(id: 2, title: "bug", description: "수정할 버그에 대한 레이블 입니다", color: "ㅇㅇ")]
     
     // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationBar()
-        configureNavigationBar()
-        configureDataSource()
-        applyInitialSnapshot()
     }
     
     private func configureNavigationBar() {
@@ -51,43 +29,6 @@ class LabelListViewController: UIViewController, UICollectionViewDelegate {
         navigationBar.isTranslucent = false
     }
     
-    private func configureCollectionView() {
-        collectionView.collectionViewLayout = createLayout()
-        collectionView.delegate = self
-    }
-    
-    private func configureDataSource() {
-        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Label> { (cell, indexPath, label) in
-            var contentConfiguration = UIListContentConfiguration.valueCell()
-            contentConfiguration.text = label.title
-            contentConfiguration.secondaryText = label.description
-            cell.contentConfiguration = contentConfiguration
-            cell.accessories = [.disclosureIndicator()]
-            
-            let star = UIImageView(image: UIImage(systemName: "star.fill"))
-            let customAceessory: UICellAccessory = .customView(configuration: .init(customView: star, placement: .leading()))
-            cell.accessories.append(customAceessory)
-            
-        }
-        
-        dataSource = UICollectionViewDiffableDataSource<Section, Label>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, label) -> UICollectionViewCell? in
-            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: label)
-        })
-        
-        let headerRegistration = UICollectionView.SupplementaryRegistration(elementKind: <#T##String#>, handler: <#T##UICollectionView.SupplementaryRegistration<UICollectionReusableView>.Handler##UICollectionView.SupplementaryRegistration<UICollectionReusableView>.Handler##(UICollectionReusableView, String, IndexPath) -> Void#>)
-    }
-    
-    private func applyInitialSnapshot() {
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Label>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(labels)
-        dataSource.apply(snapshot)
-    }
-    
-    private func createLayout() -> UICollectionViewLayout {
-        var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
-        configuration.headerMode = .supplementary
-        return UICollectionViewCompositionalLayout.list(using: configuration)
-    }
+  
 
 }
