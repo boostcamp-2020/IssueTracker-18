8188211c04068e426dac97421b73bcdff25987ad
alimelon <zhdtns2005@naver.com>
2020-11-03 14:51:42 +0900

    feat: model 객체 수정 및 추가
    - comment, emoji 모델 추가
    - model 마다 id property 추가

--- client/iOS/IssueTracker/IssueTracker/LabelListViewController.swift
+++ client/iOS/IssueTracker/IssueTracker/LabelListViewController.swift
@@ -13,6 +13,24 @@ class LabelListViewController: UIViewController, UICollectionViewDelegate {
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var collectionView: UICollectionView!
     
+    @IBAction func toucedPlusButton(_ sender: UIBarButtonItem) {
+        collectionView.isEditing = !collectionView.isEditing
+        
+//        if let currentIndexPath = self.dataSource.indexPath(for: item) {
+//            if let cell = self.collectionView.cellForItem(at: currentIndexPath) as? UICollectionViewListCell {
+//                UIView.animate(withDuration: 0.2) {
+//                    cell.accessories = self.accessoriesForListCellItem(item)
+//                }
+//            }
+//        }
+//        collectionView.visibleCells.forEach { cell in
+//            if cell == cell as? UICollectionViewListCell {
+//                cell.accessoty
+//            }
+//
+//        }
+    }
+    
     // MARK: - Properties
     var dataSource: UICollectionViewDiffableDataSource<Section, Label>!
     var labels = [Label(title: "feat", description: "기능에 대한 레이블 입니다dfafafaafafadfah", color: "ㅇㅇ"),
@@ -31,7 +49,6 @@ class LabelListViewController: UIViewController, UICollectionViewDelegate {
         navigationBar.shadowImage = UIImage()
         navigationBar.barTintColor = .systemBackground
         navigationBar.isTranslucent = false
-        navigationBar.topItem?.title = ""
     }
     
     private func configureCollectionView() {
@@ -46,11 +63,18 @@ class LabelListViewController: UIViewController, UICollectionViewDelegate {
             contentConfiguration.secondaryText = label.description
             cell.contentConfiguration = contentConfiguration
             cell.accessories = [.disclosureIndicator()]
+            
+            let star = UIImageView(image: UIImage(systemName: "star.fill"))
+            let customAceessory: UICellAccessory = .customView(configuration: .init(customView: star, placement: .leading()))
+            cell.accessories.append(customAceessory)
+            
         }
         
         dataSource = UICollectionViewDiffableDataSource<Section, Label>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, label) -> UICollectionViewCell? in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: label)
         })
+        
+        let headerRegistration = UICollectionView.SupplementaryRegistration(elementKind: <#T##String#>, handler: <#T##UICollectionView.SupplementaryRegistration<UICollectionReusableView>.Handler##UICollectionView.SupplementaryRegistration<UICollectionReusableView>.Handler##(UICollectionReusableView, String, IndexPath) -> Void#>)
     }
     
     private func applyInitialSnapshot() {
@@ -61,7 +85,8 @@ class LabelListViewController: UIViewController, UICollectionViewDelegate {
     }
     
     private func createLayout() -> UICollectionViewLayout {
-        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        configuration.headerMode = .supplementary
         return UICollectionViewCompositionalLayout.list(using: configuration)
     }
 
---
+++ client/iOS/IssueTracker/IssueTracker/Model/Comment.swift
@@ -0,0 +1,17 @@
+//
+//  Comment.swift
+//  IssueTracker
+//
+//  Created by A on 2020/11/03.
+//
+
+import Foundation
+
+struct Comment: Codable, Hashable {
+    let id: Int
+    let creater: User
+    let createAt: String
+    let updateAt: String
+    let content: String
+    let emogi: [Emoji]?
+}
