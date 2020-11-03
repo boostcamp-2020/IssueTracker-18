8be2a1c87c9406e5ff0ecd8e041bffcd13acc04c
grap3fruit <swkwon333@gmail.com>
2020-11-03 17:43:59 +0900

    feat: error handler app.js에 추가

--- server/app.js
+++ server/app.js
@@ -15,3 +15,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api', indexController);
 
 app.listen(8080);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'production' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
