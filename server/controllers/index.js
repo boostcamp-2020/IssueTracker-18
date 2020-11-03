4299c948240ac29d26e2153a82d5cf39dae50f94
sukstar76 <sukstar76@gmail.com>
2020-11-03 12:02:12 +0900

    fix : 오탈자 수정
    warpper 를 wrapper 로 수정

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 // const userController = require('@controllers/user');
 const issueController = require('@controllers/issue');
+const commentController = require('@controllers/comment');
+const emojiController = require('@controllers/emoji');
 
 router.use('/issue', issueController);
+router.use('/comment', commentController);
+router.use('/emoji', emojiController);
 
 module.exports = router;
