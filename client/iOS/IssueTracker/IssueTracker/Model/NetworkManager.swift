53f50001798a3ed0ca369b38292e341330fda47e
alimelon <zhdtns2005@naver.com>
2020-11-05 16:57:22 +0900

    feat: NetworkManager 객체 생성
    - string URL로 jsonData 받아오는 getData() 구현

--- client/iOS/IssueTracker/IssueTracker/Model/NetworkManager.swift
+++ client/iOS/IssueTracker/IssueTracker/Model/NetworkManager.swift
@@ -12,7 +12,18 @@ class NetworkManager {
     func getData(url: String, completion: @escaping (Data)->()) {
         let alamo = AF.request(url, method: .get).validate(statusCode: 200..<300)
         alamo.responseJSON { (response) in
-            
+            switch response.result {
+            case .success(let value):
+                do {
+                    let data = try JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+                    completion(data)
+                } catch {
+                    
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
         }
     }
     
