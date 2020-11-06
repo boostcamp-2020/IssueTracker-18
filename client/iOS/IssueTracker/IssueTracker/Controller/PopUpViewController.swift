e7f215c9a56f7b9e1a383b02f808a88136377d4f
= <yeonduing@gmail.com>
2020-11-07 00:23:43 +0900

    feat: 필수 항목을 채우지 않았을 경우 진동 및 해당 textfield의 placeholder 색 변경

--- client/iOS/IssueTracker/IssueTracker/Controller/PopUpViewController.swift
+++ client/iOS/IssueTracker/IssueTracker/Controller/PopUpViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class PopUpViewController: UIViewController {
     
@@ -26,7 +27,7 @@ class PopUpViewController: UIViewController {
     private func configurePopUpView() {
         guard let popUpViewWarpper = self.popUpViewWrapper as? PopUpViewWrapper else { return }
         popUpView = popUpViewWarpper.contentView
-        popUpView?.saveButton.addTarget(self, action: #selector(dismissSelf), for: .touchUpInside)
+        popUpView?.saveButton.addTarget(self, action: #selector(saveTexts), for: .touchUpInside)
         popUpView?.cancelButton.addTarget(self, action: #selector(dismissSelf), for: .touchUpInside)
         popUpView?.resetButton.addTarget(self, action: #selector(resetTextField), for: .touchUpInside)
         
@@ -40,6 +41,37 @@ class PopUpViewController: UIViewController {
         popUpView?.titleTextField.text = ""
         popUpView?.secondTextField.text = ""
         popUpView?.lastTextField.text = ""
+    }
+    
+    @objc private func saveTexts() {
+        guard let popUpView = popUpView else { return }
+        let title = popUpView.titleTextField.text ?? ""
+        let secondText = popUpView.secondTextField.text ?? ""
+        let lastText = popUpView.lastTextField.text ?? ""
+        
+        if( configureVibrateAlert(popUpView, title, secondText, lastText) ) {
+            return
+        }
+    }
+    
+    private func configureVibrateAlert(_ popUpView: PopUpView,
+                                       _ title: String,
+                                       _ secondText: String,
+                                       _ lastText: String) -> Bool {
+        if(title.isEmpty) {
+            popUpView.titleTextField.configurePlaceholderColor(color: UIColor.systemRed)
+        }
+        if(secondText.isEmpty) {
+            popUpView.secondTextField.configurePlaceholderColor(color: UIColor.systemRed)
+        }
+        if(lastText.isEmpty) {
+            popUpView.lastTextField.configurePlaceholderColor(color: UIColor.systemRed)
+        }
+        if(title.isEmpty || secondText.isEmpty || lastText.isEmpty) {
+            AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+            return true
+        }
+        return false
     }
     
 }
