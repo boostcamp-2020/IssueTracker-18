06bb6c734c93b29b313f22204448ece0ae82fc77
= <yeonduing@gmail.com>
2020-11-06 10:52:28 +0900

    feat: badgeLabel background color를 systemBackground로 변경하여 다크모드 대응

--- client/iOS/IssueTracker/IssueTracker/BadgeLabel.swift
+++ client/iOS/IssueTracker/IssueTracker/BadgeLabel.swift
@@ -75,6 +75,7 @@ import UIKit
         case .label:
             return
         case .milestone:
+            self.backgroundColor = UIColor.systemBackground
             layer.borderWidth = 1
             layer.borderColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         }
