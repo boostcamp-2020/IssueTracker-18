2d704d30e95ddc8cdf9567c7a97db0f5ea04ca46
grap3fruit <swkwon333@gmail.com>
2020-11-09 10:43:12 +0900

    fix: GET comment 버그 수정

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -15,6 +15,7 @@ router.get(
       include: [
         {
           model: models.emoji,
+          as: 'emojis',
           attributes: ['id', 'name', 'imageUrl'],
           through: { attributes: [] },
         },
