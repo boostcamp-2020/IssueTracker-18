fde45767c40648dcb88ccd92f75bf54b4db66ac8
= <yeonduing@gmail.com>
2020-11-12 14:46:28 +0900

    feat: swiftUI 버전의 bottom sheet view 생성 및 스토리보드와 연결

---
+++ client/iOS/IssueTracker/IssueTracker/SwiftUI/BottomSheetView.swift
@@ -0,0 +1,98 @@
+//
+//  BottomSheetView.swift
+//  IssueTracker
+//
+//  Created by 류연수 on 2020/11/11.
+//
+
+import SwiftUI
+
+struct BottomSheetView: View {
+    @State var showingNewComment = false
+    var body: some View {
+        
+        VStack {
+            Capsule()
+                .fill(Color.gray.opacity(0.5))
+                .frame(width: 50, height: 5)
+                .padding(.top)
+                .padding(.bottom, 5)
+            
+            HStack(spacing: 15){
+                Button(action: {
+                    self.showingNewComment.toggle()
+                }) {
+                    HStack {
+                        Spacer()
+                        Text("댓글 추가")
+                        Spacer()
+                    }
+                }.sheet(isPresented: $showingNewComment) {
+                    NewCommentView()
+                }.buttonStyle(RoundedRectangleButtonStyle())
+                
+                HStack(spacing: 0) {
+                    Button(action: {
+                        self.showingNewComment.toggle()
+                    }) {
+                        Image(systemName: "chevron.up")
+                    }.sheet(isPresented: $showingNewComment) {
+                        NewCommentView()
+                    }.padding(.trailing, 10)
+                    
+                    Button(action: {
+                        self.showingNewComment.toggle()
+                    }) {
+                        Image(systemName: "chevron.down")
+                    }.sheet(isPresented: $showingNewComment) {
+                        NewCommentView()
+                    }.padding(.leading, 10)
+                    
+                }.padding()
+                .cornerRadius(3.0)
+                .border(Color(UIColor.systemGray3))
+                .background(Color(UIColor.systemBackground))
+                
+            }
+            .padding(.horizontal)
+            .cornerRadius(15)
+            
+            ScrollView(.vertical, showsIndicators: false, content: {
+                LazyVStack(alignment: .leading, spacing: 15, content: {
+                    ForEach(1...10, id: \.self) { count in
+                        AssigneeView("hi")
+                        
+                        LabelView(Label(id: nil, title: "iOS", description: nil, color: "#F01932"))
+                        Divider()
+                            .padding(.top, 10)
+                    }
+                })
+                .padding()
+                .padding(.top)
+            })
+        }
+        .background(BlurView(style: .systemMaterial))
+        .cornerRadius(15)
+    }
+}
+struct BottomSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomSheetView()
+    }
+}
+
+struct RoundedRectangleButtonStyle: ButtonStyle {
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(.vertical, 10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 3.0)
+                    .stroke(Color(UIColor.systemGray3), lineWidth: 1)
+            )
+            .foregroundColor(configuration.isPressed ? Color.blue.opacity(0.3) : Color.blue
+            )
+            .background(Color(UIColor.systemBackground))
+    }
+    
+}
