e65fd09670855dc3ea5b87afd20afb7a02247379
soonwon <cliegh@naver.com>
2020-11-12 19:04:24 +0900

    feat: 이슈목록 업데이트 시간 추가

--- client/web/src/atoms/IssueAuthor.js
+++ client/web/src/atoms/IssueAuthor.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledIssueAuthor = styled.div`
+  padding-left: 5px;
+`;
 
 const IssueAuthor = ({ issueId, creater }) => {
-  return <div className="IssueAuthor">{creater.name}</div>;
+  return <StyledIssueAuthor className="IssueAuthor">{creater.name}</StyledIssueAuthor>;
 };
 
 export default IssueAuthor;
--- client/web/src/atoms/IssueMetaInfo.js
+++ client/web/src/atoms/IssueMetaInfo.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import changeTime from '../utils/changeTime';
 
 const IssueMetaInfo = ({ issueId, isOpen, updatedAt = '1 days ago' }) => {
   return (
     <div>
-      {issueId} {isOpen === true ? 'opened' : 'closed'} {updatedAt}
+      {issueId} {isOpen === true ? 'opened' : 'closed'}{' '}
+      {changeTime(new Date(), new Date(updatedAt))}
     </div>
   );
 };
