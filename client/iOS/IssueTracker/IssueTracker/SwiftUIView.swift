4dd7dafd68695ee7959e397de40c6ff166152371
= <yeonduing@gmail.com>
2020-11-13 01:32:29 +0900

    fix: 안쓰는 뷰 삭제

--- client/iOS/IssueTracker/IssueTracker/SwiftUIView.swift
+++ client/iOS/IssueTracker/IssueTracker/SwiftUIView.swift
@@ -152,60 +152,6 @@ struct SwiftUIView: View {
 //    }
 //}
 
-struct BottomSheet: View {
-    
-    @State var txt = ""
-    @Binding var offset: CGFloat
-    var value: CGFloat
-    var body: some View {
-        
-        VStack {
-            Capsule()
-                .fill(Color.gray.opacity(0.5))
-                .frame(width: 50, height: 5)
-                .padding(.top)
-                .padding(.bottom, 5)
-            
-            HStack(spacing: 15){
-                Image(systemName: "magnifyingglass")
-                    .font(.system(size: 22))
-                    .foregroundColor(.gray)
-                
-                TextField("Search Place", text: $txt) { (status) in
-                    withAnimation {
-                        offset = value
-                    }
-                } onCommit: {
-                    
-                }
-                
-            }
-            .padding(.vertical, 10)
-            .padding(.horizontal)
-            //blur view
-            //for Dark Mode Adoption
-            .background(BlurView(style: .systemMaterial))
-            .cornerRadius(15)
-            .padding()
-            
-            ScrollView(.vertical, showsIndicators: false, content: {
-                LazyVStack(alignment: .leading, spacing: 15, content: {
-                    ForEach(1...10, id: \.self) { count in
-                        Text("Placeholder \(count)")
-                        
-                        Divider()
-                            .padding(.top, 10)
-                    }
-                })
-                .padding()
-                .padding(.top)
-            })
-        }
-        .background(BlurView(style: .systemMaterial))
-        .cornerRadius(15)
-    }
-}
-
 struct BlurView: UIViewRepresentable {
     
     var style: UIBlurEffect.Style
